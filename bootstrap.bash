57738d31c6339b6b15fbe76ee9503a307edcb1ea
R.A. Lucas <richard@richardalucas.com>
2021-10-10 17:58:17 -0600

    fixed and working, tested on 32bit debian

--- bootstrap.bash
+++ bootstrap.bash
@@ -1,33 +1,37 @@
 #!/bin/bash -ex
 
 # VERSIONS
-go_ver="1.15"
-node_ver="12.18.3"
+go_ver="1.17.2"
 
-os=`uname -s`
+os=$(uname -s | tr "[:upper:]" "[:lower:]")
+os_arch="64"
+
+if [[ "$(arch)" != *"64"* ]]; then
+  os_arch="32"
+fi
 
 pkgmgr() {
   sudo dnf -y "$@"
 }
 
 distro="fedora"
-wm=""
+window_mgr=""
 
-if [[ $os == "Darwin" ]]; then
+if [[ $os == "darwin" ]]; then
   pkgmgr() {
     brew "$@"
   }
   distro="mac"
-  wm="mac"
-elif [[ $os == "Linux" ]]; then
-  wm=`echo $XDG_CURRENT_DESKTOP | tr '[:upper:]' '[:lower:]'`
-  apt=`grep -ioh -e ubuntu -e debian /etc/*release`
+  window_mgr="mac"
+elif [[ $os == "linux" ]]; then
+  window_mgr=$(echo "$XDG_CURRENT_DESKTOP" | tr "[:upper:]" "[:lower:]")
+  apt=$(grep -ioh -e ubuntu -e debian /etc/*release)
   if [[ "$apt" ]]; then
-    echo "Using apt-get"
+    echo "Using apt"
     pkgmgr() {
-      sudo apt-get -y "$@"
+      sudo apt -y "$@"
     }
-    distro="$apt"
+  distro=$(echo "$apt" | head -1 | tr "[:upper:]" "[:lower:]")
   else
     echo "Using dnf"
   fi
@@ -35,110 +39,127 @@ fi
 
 dotfiles_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+echo "Running script"
+echo "---------------"
+echo "OS: $os"
+echo "Distro: $distro"
+echo "Windows Manager: $window_mgr"
+echo "Architecture: $os_arch-bit"
+echo "---------------"
+
 # BASICS
-pkgmgr update
-pkgmgr install -y git curl zlib1g-dev build-essential libssl-dev libreadline-dev \
-  libyaml-dev libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties \
-  libffi-dev libgdbm-dev libncurses5-dev automake libtool bison libffi-dev ruby-full
-
-# GIT SETUP
-git config --global color.ui true
-mv git-templates ~/.git-templates
-chmod a+x ~/.git-templates/hooks/*
-
-# VIM SETUP
-pkgmgr install -y vim
-mkdir -p ~/.vim/bundle
-mkdir -p ~/.vim/colors
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp ${dotfiles_dir}/.basicvimrc ~/.vimrc
-vim +PluginInstall +qall
-cp ~/.vim/bundle/vim-colors-solarized/colors/solarized.vim ~/.vim/colors/
-
-# NODE.JS
-curl -O https://nodejs.org/dist/v${node_ver}/node-v${node_ver}-linux-x64.tar.xz
-tar -xvf node*.tar.xz -C /usr/local
-
-# RUBY ON RAILS
-gem install rails
-gem install bundler
-
-pkgmgr update
-
-# GO
-curl -O https://storage.googleapis.com/golang/go${go_ver}.linux-amd64.tar.gz
-tar -C /usr/local -xvf go*.tar.gz
-echo -e "\n# GO" | tee -a ~/.bash_profile
-echo "export GOPATH=$HOME/go" | tee -a ~/.bash_profile
-echo "export GOBIN=$GOPATH/bin" | tee -a ~/.bash_profile
-echo "export PATH=$PATH:$GOPATH/bin" | tee -a ~/.bash_profile
-
-# ERLANG & ELIXIR
-pkgmgr install -y erlang
-pkgmgr install -y elixir
-
-# LISP
-pkgmgr install -y clisp
-
-# MYSQL
-pkgmgr install -y mysql-client
-pkgmgr install -y mysql-server mysql-community-server
-
-# POSTGRES
-pkgmgr install -y postgresql-server
-echo -e "\n# POSTGRES\nexport PGDATA=/usr/local/var/postgres" >> ~/.bash_profile
-
-# REDIS
-wget -P /opt/ http://download.redis.io/releases/redis-stable.tar.gz
-tar xzf /opt/redis-stable.tar.gz
-cd /opt/redis-stable
-make
-ln -s /opt/redis-stable/src/redis-server /usr/local/bin/redis-server
-# TODO: add redis to auto start on startup
-
-# MONGODB
-pkgmgr install -y mongodb mongodb-server
-
-# NGINX
-pkgmgr install -y nginx-full
-
-# SET CAPS LOCK TO CTRL
-setxkbmap -layout us -option ctrl:nocaps
-
-# SETUP FOR VSCODE
-if [[ "$vscode" -eq 1 ]]; then
-  sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-  sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-  dnf check-update
-  dnf install code
+if [[ $distro != "mac" ]]; then
+  echo "Updating and installing packages"
+  pkgmgr update
+  pkgmgr install git curl zsh build-essential automake shellcheck python3 python3-pip
 fi
 
-# TEXMAKER
-pkgmgr install -y texmaker
+# GIT SETUP
+echo "setting up git..."
+cp .gitconfig "$HOME"/.gitconfig
+cp -R git-templates "$HOME"/.git-templates
+chmod a+x "$HOME"/.git-templates/hooks/*
+
+## VIM SETUP
+echo "installing vim..."
+pkgmgr install vim
+mkdir -p "$HOME"/.vim/bundle
+mkdir -p "$HOME"/.vim/colors
+git clone https://github.com/VundleVim/Vundle.vim.git "$HOME"/.vim/bundle/Vundle.vim
+cp "${dotfiles_dir}/.basicvimrc_starter" "$HOME"/.vimrc
+vim +PluginInstall +qall
+cp "$HOME"/.vim/bundle/vim-colors-solarized/colors/solarized.vim "$HOME"/.vim/colors/
+cat "${dotfiles_dir}/.basicvimrc_starter" "${dotfiles_dir}/.basicvimrc_end" > .vimrc
+mv .vimrc "$HOME"/.vimrc
+
+# INSTALL prezto
+echo "installing prezto..."
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto" || \
+  (cd "${ZDOTDIR:-$HOME}"/.zprezto && git restore . && git pull --recurse-submodules && cd "${dotfiles_dir}") 
+mv "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/zshrc "${ZDOTDIR:-$HOME}"/.zshrc
+for rcfile in $(ls "${ZDOTDIR:-$HOME}"/.zprezto/runcoms); do
+  if [[ "$rcfile" != "README*" ]]; then
+    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}" || true
+  fi
+done
+
+# INSTALL pure theme
+echo "installing pure theme..."
+mkdir -p "$HOME/.zsh"
+git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure" || \
+  (cd "$HOME/.zsh/pure" && git pull && cd "${dotfiles_dir}") 
+cat <<EOT >> "$HOME/.zshrc"
+## Pure Prompt
+fpath+=\$HOME/.zsh/pure
+autoload -U promptinit; promptinit
+prompt pure
+EOT
+
+# Update zsh history config
+echo "" >> "$HOME/.zshrc"
+cat <<EOT >> "$HOME/.zshrc"
+################################################################################################
+## History Configuration
+################################################################################################
+HISTSIZE=1000000
+HISTFILE=\$HOME/.zsh_history
+SAVEHIST=1000000
+setopt BANG_HIST                 # Treat the '!' character specially during expansion.
+setopt EXTENDED_HISTORY          # Write the history file in the ":start:elapsed;command" format.
+setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
+setopt SHARE_HISTORY             # Share history between all sessions.
+setopt HIST_EXPIRE_DUPS_FIRST    # Expire duplicate entries first when trimming history.
+setopt HIST_IGNORE_DUPS          # Don't record an entry that was just recorded again.
+setopt HIST_IGNORE_ALL_DUPS      # Delete old recorded entry if new entry is a duplicate.
+setopt HIST_FIND_NO_DUPS         # Do not display a line previously found.
+setopt HIST_IGNORE_SPACE         # Don't record an entry starting with a space.
+setopt HIST_SAVE_NO_DUPS         # Don't write duplicate entries in the history file.
+setopt HIST_REDUCE_BLANKS        # Remove superfluous blanks before recording entry.
+setopt HIST_VERIFY               # Don't execute immediately upon history expansion.
+setopt HIST_BEEP                 # Beep when accessing nonexistent history.]
+
+EOT
+
+# GO
+echo "installing go..."
+go_arch="amd64"
+if [[ "$os_arch" == "32" ]]; then
+  go_arch="386"
+fi
+curl -O "https://storage.googleapis.com/golang/go${go_ver}.${os}-${go_arch}.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xvf go*.tar.gz
+mkdir -p "$HOME"/.go
+GOPATH="$HOME"/.go
+echo "" >> "$HOME/.zshrc"
+cat <<EOT >> "$HOME/.zshrc"
+## GO
+export PATH=\$PATH:/usr/local/go/bin
+export GOPATH=$GOPATH
+export GOBIN=\$GOPATH/bin
+export PATH=\$PATH:\$GOPATH/bin
+EOT
+sudo rm "${dotfiles_dir}"/go*
 
 # MONITORING
-pip install s-tui --user
-pkgmgr install -y htop stress
-
-echo "PATH=$PATH:/.local/bin" >> .bash_profile
-
-## TODO:
-# 1. Install presto, zsh, and pure theme
-# 2. Install anaconda for python
-# 3. Install kafka, zookeeper, and prometheus
-# 4. Install spark and hadoop
-# 5. Install java and scala
+echo "installing monitoring tools..."
+pip3 install s-tui --user
+pkgmgr install htop stress
+echo "PATH=\$PATH:/.local/bin" >> "$HOME"/.zshrc
 
 # DISTRO SPECIFIC
-if [[ ${dotfiles_dir}/${wm}_packages.txt ]]; then
-  for pkg in `cat ${dofiles_dir}/${distro}_packages.txt`; do
-    pkgmgr install $pkg;
+echo "installing distro specific packages..."
+echo "${dotfiles_dir}/${window_mgr}_packages.txt"
+if [[ -f "${dotfiles_dir}/${window_mgr}_packages.txt" ]]; then
+  echo "installing packages..."
+  pkgs=($(cat "${dotfiles_dir}/${window_mgr}_packages.txt" | tr "\n" " "))
+  for pkg in ${pkgs[@]}; do
+    echo "installing $pkg"
+    pkgmgr install "$pkg"
   done
 fi
 
-# CLEANUP
-rm ${dotfiles_dir}/go*
-rm -rf ${dotfiles_dir}/git
-pkgmgr update
-
+chsh -s /bin/zsh
+zsh
+source "$HOME"/.zshrc
 
